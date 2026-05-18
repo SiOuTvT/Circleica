c062bf61b51831cc6f8b960f683ee00f1774dd6e
四隅 <siyu0721111@qq.com>
2026-05-18 17:05:12 +0800

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,10 @@ module.exports = {
   apps: [
     {
       name: "fangame",
-      script: "node_modules/.bin/next",
+      script: "node_modules/next/dist/bin/next",
       args: "start",
       cwd: "/www/wwwroot/fangame",
+      interpreter: "node",
       instances: 1,
       autorestart: true,
       watch: false,
