b8743f302f3d41a8c6fde7858fcc2d7777a5d6ec
四隅 <siyu0721111@qq.com>
2026-06-04 05:30:29 +0800

    Update announce-swiper.tsx

--- src/components/announce-swiper.tsx
+++ src/components/announce-swiper.tsx
@@ -61,7 +61,7 @@ export function AnnounceSwiper({ announcements }: { announcements: Ann[] }) {
 
   return (
     <div
-      className="relative aspect-video w-full sm:max-w-[60%] sm:ml-auto overflow-hidden rounded-2xl bg-zinc-900 light:bg-zinc-100"
+      className="relative aspect-video w-full sm:max-w-[60%] sm:ml-auto overflow-hidden rounded-2xl bg-card"
       onMouseEnter={() => setPaused(true)}
       onMouseLeave={() => setPaused(false)}
     >
