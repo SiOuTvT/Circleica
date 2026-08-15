f9258d9cdc7edf3593d4ffc4d3a6e947c847de44
四隅 <siyu0721111@qq.com>
2026-08-16 03:49:06 +0800

    删除

--- prisma/_tmp_ws_check.mjs
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const p = new PrismaClient();
-const r = await p.$queryRawUnsafe(`SELECT COUNT(*)::int AS c FROM (SELECT "source","externalId" FROM "WorkSource" GROUP BY "source","externalId" HAVING COUNT(*)>1) x`);
-console.log('WorkSource (source,externalId) duplicate groups =>', r[0].c);
-const r2 = await p.$queryRawUnsafe(`SELECT COUNT(*)::int AS c FROM (SELECT "workId","source" FROM "WorkSource" GROUP BY "workId","source" HAVING COUNT(*)>1) x`);
-console.log('WorkSource (workId,source) duplicate groups =>', r2[0].c);
-await p.$disconnect();
