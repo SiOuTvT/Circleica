e121094a82c27797e96f2e1872848de303315c9a
四隅 <siyu0721111@qq.com>
2026-08-18 04:09:58 +0800

    Remove generated build artifacts and repro files
    Remove accidentally committed/generated files: _repro_out.js (bundled output), _build_log.txt, _run_build.ps1, _start_dev.ps1, and _ut_repro.ts. Cleans up large/temporary build and reproduction artifacts from the repository.

--- _run_build.ps1
+++
@@ -1,8 +0,0 @@
-$ErrorActionPreference = 'Continue'
-$env:CODEBUDDY_SAFE_DELETE_ENABLED = '0'
-$log = 'd:\Circleica\_build_phase0.log'
-$err = 'd:\Circleica\_build_phase0.err'
-Start-Process -FilePath 'cmd.exe' `
-  -ArgumentList '/c', "cd /d d:\Circleica && npm run build > $log 2>$err" `
-  -WindowStyle Hidden `
-  -PassThru | Select-Object Id
--- _start_dev.ps1
+++
@@ -1,3 +0,0 @@
-$env:CODEBUDDY_SAFE_DELETE_ENABLED = '0'
-Start-Process -FilePath 'cmd' -ArgumentList '/c','cd /d d:\Circleica && npm run dev > d:\Circleica\_dev3.log 2>&1' -WindowStyle Hidden
-Write-Output 'STARTED with CODEBUDDY_SAFE_DELETE_ENABLED=0'
